fe392369fc9f7f655998fe1eeeeb011df10e7118
Israel Rivas <israelrivasgonzalez@gmail.com>
2019-03-26 11:41:43 +0100

    Adding spec to AudioQualitiesMovie model

--- spec/models/audio_qualities_movie_spec.rb
+++ spec/models/audio_qualities_movie_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe AudioQualitiesMovie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_uniqueness_of(:audio_quality_id).scoped_to(:movie_id) }
+  it { is_expected.to belong_to(:audio_quality) }
+  it { is_expected.to belong_to(:movie) }
+
+  it { is_expected.to have_attribute :audio_quality_id }
+  it { is_expected.to have_attribute :movie_id }
+
 end
